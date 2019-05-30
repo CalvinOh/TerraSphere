0b5a5702ebf2a97909ee52d3401a146690ac6c87
CalvinOh <calvinohwyekit@gmail.com>
2019-05-30 18:14:47 -0500

    TERA Programming Test

---
+++ Tera Testing/Assets/Script/GravityBody.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : MonoBehaviour
+{
+
+    public GravityAttractor gravityAttractor;
+    private Transform myTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        this.GetComponent<Rigidbody>().useGravity = false;
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gravityAttractor.Attract(myTransform);
+    }
+}
---
+++ Tera Testing/Assets/Script/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+
+    private Vector3 moveDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        this.GetComponent<Rigidbody>().MovePosition(this.GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+    }
+}
