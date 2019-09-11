44df7a7b7fb0fc019567f19ed7133ea9d46e51d9
Pedro A. Tresgallo <P3dro115@gmail.com>
2019-09-10 22:21:07 -0500

    Add groundwork
    Add scrolling to inventoryTest UI.
    I key toggles inventory on/ off in prototype
    
    Add Item script to hold an individual item's information
    Add Slot script to hold the item's information for the UI
    Add Inventory script logic to populate slots' item information

---
+++ Tera Testing/Assets/Script/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int ID;
+    public string type;
+    public string description;
+    public Texture2D icon;
+    public bool pickedUp;
+
+
+}
---
+++ Tera Testing/Assets/Script/Slot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    public GameObject item;
+    public int ID;
+    public string type;
+    public string description;
+    public bool empty;
+    public Texture2D icon;
+
+    public void UpdateSlot()
+    {
+
+    }
+}
