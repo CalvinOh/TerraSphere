5691070bddd6cde9f7f75702a27556cc7a34afbe
CalvinOh <calvinohwyekit@gmail.com>
2019-09-18 21:06:56 -0500

    Adds functions for each item
    items only trigger on specific tag, player can recognise what they are touching, though still buggy

--- Tera Testing/Assets/Script/PlayerController.cs
+++ Tera Testing/Assets/Script/PlayerController.cs
@@ -15,13 +15,21 @@ public class PlayerController : MonoBehaviour
     int itemInInventorySelected;
 
     [SerializeField]
-    Transform spawnLocation;
+    Transform spawnHeldLocation;
+    [SerializeField]
+    Transform spawnItemLocation;
     [SerializeField]
     GameObject currentlyHolding;
     [SerializeField]
     GameObject blankSlot;
     [SerializeField]
     private float jumpHeight;
+    [SerializeField]
+    private GameObject currentlySelecting;
+
+    private GameObject planet;
+
+    public List<GameObject> objectsInTrigger;
 
     private Vector3 moveDirection;
     private bool itemSelected;
@@ -31,6 +39,11 @@ public class PlayerController : MonoBehaviour
     {
         //inventory = new GameObject[10];
         currentlyHolding = inventory[0];
+        if(planet = null)
+        {
+            planet = FindObjectOfType<GravityAttractor>().gameObject;
+        }
+        objectsInTrigger = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -53,8 +66,8 @@ public class PlayerController : MonoBehaviour
     private void DisplayItemSelected()
     {
         // Assigning the currently held item
-        currentlyHolding = (GameObject)Instantiate(inventory[itemInInventorySelected], spawnLocation.transform.position, spawnLocation.transform.rotation);
-        currentlyHolding.transform.parent = spawnLocation.transform;
+        currentlyHolding = (GameObject)Instantiate(inventory[itemInInventorySelected], spawnHeldLocation.transform.position, spawnHeldLocation.transform.rotation);
+        currentlyHolding.transform.parent = spawnHeldLocation.transform;
         //currentlyHolding.transform.position = spawnLocation.transform.position;
         //GameObject item = (GameObject)Instantiate(currentlyHolding, spawnLocation.transform);
         //item.transform.parent = this.transform;
@@ -137,23 +150,65 @@ public class PlayerController : MonoBehaviour
 
         if(Input.GetButtonDown("Fire1"))
         {
-            //Seed, 
+            //When left click, based on item held, trigger function
+            if (currentlyHolding.CompareTag("Shovel"))
+            {
+                DigGround();
+            }
             if (currentlyHolding.CompareTag("Seed"))
             {
                 PlantSeed();
             }
+            if (currentlyHolding.CompareTag("Rake"))
+            {
+                CoverSeed();
+            }
+            if (currentlyHolding.CompareTag("WateringCan"))
+            {
+                Water();
+            }
+        }
+    }
+
+    private void Water()
+    {
+        if (currentlySelecting.CompareTag("Seeded") || currentlySelecting.CompareTag("Plant"))
+        {
+            //Trigger plant accelarate function
+        }
+    }
+
+    private void CoverSeed()
+    {
+        if (currentlySelecting.CompareTag("Seed"))
+        {
+            //Trigger plant growth function plus cover up seed 
+            print("Raking...");
+        }
+    }
+
+    private void DigGround()
+    {
+        if(currentlySelecting.CompareTag("Ground"))
+        {
+            //Trigger change in ground
+            //Spawn dug ground on floor
+            print("Digging");
         }
     }
 
     void PlantSeed()
     {
-        //Spawn a new seed/plant onto the world, and starts the plant growth cycle, removes a stack until 0 and removes from inventory.
-        GameObject newSeed = (GameObject)Instantiate(inventory[itemInInventorySelected], spawnLocation.transform.position, spawnLocation.transform.rotation);
-        blankSlot = new GameObject();
-        inventory[itemInInventorySelected] = blankSlot;
-        currentlyHolding = inventory[itemInInventorySelected];
+        if(currentlySelecting.CompareTag("Dug"))
+        {
+            //Spawn a new seed/plant onto the world, and starts the plant growth cycle, removes a stack until 0 and removes from inventory.
+            GameObject newSeed = (GameObject)Instantiate(inventory[itemInInventorySelected], spawnItemLocation.transform.position, spawnItemLocation.transform.rotation);
+            blankSlot = new GameObject();
+            inventory[itemInInventorySelected] = blankSlot;
+            currentlyHolding = inventory[itemInInventorySelected];
 
-        //check for stack, if yes -- from stack; else remove from inventory;
+            //check for stack, if yes -- from stack; else remove from inventory;
+        }
 
     }
 
@@ -161,5 +216,21 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         hasJumped = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        objectsInTrigger.Add(other.gameObject);
+        currentlySelecting = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        objectsInTrigger.Remove(other.gameObject);
+
+        if(objectsInTrigger.Count == 0 || (objectsInTrigger.Count == 1 && objectsInTrigger[0] == planet) )
+        {
+            currentlySelecting = planet;
+        }
     }
 }
