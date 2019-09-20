f6df30d53ab6108ef7e7da37c6207fd50f88054f
ThisisVickyZ <55002065+ThisisVickyZ@users.noreply.github.com>
2019-09-19 19:35:41 -0500

    Some new shaders added, outline for plants, comets, rocket particles
    There are many failed attempts at the cell shader in here, keeping them in incase I get one to work within the next week or so

---
+++ Tera Testing/Assets/toonShaderScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toonShaderScript : MonoBehaviour
+{
+    //Victoria wrote this. I have no idea how to code, if I made a mistake please let me know
+    private Light light = null;
+
+    private void OnEnable()
+    {
+        light = this.GetComponent<Light>();
+    }
+    private void Update()
+    {
+        Shader.SetGlobalVector("_ToonLightDirection",-this.transform.forward):
+    }
+}
