a2f097eb6de5f8b5b959a5f051924c5a3c7da272
TheQuestCrafter <thequestcrafter@gmail.com>
2019-09-26 21:18:54 -0500

    Revert "Revert "UI Updates""
    This reverts commit 42cc99a85677c9749dac25e41e8603f8ebceecc6.

---
+++ Tera Testing/Assets/Script/UI/GameOverMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The event system which handles currently selected or hovered UI option, important for keyboard or gamepad only.")]
+    private EventSystem eventSystem;
+
+    [SerializeField]
+    [Tooltip("The darkened panel containing the game over UI so when set active, all the UI becomes visible.")]
+    private GameObject gameOverPanel;
+
+    [SerializeField]
+    [Tooltip("The button the gameover panel should start on.")]
+    private Button startingButton;
+
+    [SerializeField]
+    [Tooltip("The scene index number for this scene.")]
+    private int thisSceneIndex;
+
+    public void DeathScreen()
+    {
+        gameOverPanel.SetActive(true);
+        eventSystem.SetSelectedGameObject(startingButton.gameObject);
+        //activates death screen panel and sets the starting button to be selected entering the player into that menu navigation.
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(thisSceneIndex);
+    }
+}
