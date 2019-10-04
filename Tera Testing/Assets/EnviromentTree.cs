543962ae565f0dd251da95e09ad8bf68e9600ce5
Jiening <Jiening.tao@loop.colum.edu>
2019-10-03 23:27:19 -0500

    worked on trees, have a close to finished bounce

--- Tera Testing/Assets/EnviromentTree.cs
+++ Tera Testing/Assets/EnviromentTree.cs
@@ -8,6 +8,8 @@ public class EnviromentTree : MonoBehaviour
     private float PopupSize;
 
 
+
+    private Vector3 StartSize;
     private float CurrentScale;
 
     private float StartTime;
@@ -19,23 +21,37 @@ public class EnviromentTree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartTime = 0;
+        StartSize = transform.localScale;
+        CurrentScale = 0.1f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        DeterminCurrentSize();
+        if (CurrentScale < 1f)
+            DeterminCurrentSize();
 
-        this.transform.localScale =new Vector3( 7 * CurrentScale,7*CurrentScale,7*CurrentScale);
+        this.transform.localScale = StartSize * CurrentScale;
     }
 
 
     void DeterminCurrentSize()
     {
-        CurrentScale = -(20 * Mathf.Sin(20*(Time.fixedTime - StartTime))) / (20*(Time.fixedTime - StartTime)) + 10;
-        CurrentScale = Mathf.Clamp(CurrentScale, 0, 1000000);
+        // CurrentScale *= 1.01f;
+        //CurrentScale = (-(20 * Mathf.Sin(20 * (Time.fixedTime - StartTime))) / (20 * (Time.fixedTime - StartTime)) + 10) / 10 * PopupSize;
+        
+        if (Time.fixedTime - StartTime >= 0.824)
+        {
+            CurrentScale *= 1.00005f;
+        }
+        else
+        {
+            CurrentScale = (-(20 * Mathf.Sin(30 * (Time.fixedTime - StartTime))) / (60 * (Time.fixedTime - StartTime)) + 10)/10 * PopupSize;
+        }
+        
 
+        CurrentScale = Mathf.Clamp(CurrentScale, 0f, 1f);
     }
 
 
