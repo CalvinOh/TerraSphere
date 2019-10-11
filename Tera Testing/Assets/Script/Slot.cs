2153b978d48116d13c561222008b983007722045
CalvinOh <calvinohwyekit@gmail.com>
2019-10-11 14:59:19 -0500

    Controller Fix

--- Tera Testing/Assets/Script/Slot.cs
+++ Tera Testing/Assets/Script/Slot.cs
@@ -65,8 +65,8 @@ public class Slot : MonoBehaviour, IPointerClickHandler
             else if (item.GetComponent<Item>().type == "Seed")
             {
 
-                this.player.GetComponent<PlayerController>().hotBarInventory[1] = item;
-                this.player.GetComponent<PlayerController>().seedSlotBackground.GetComponent<Image>().sprite = item.GetComponent<Item>().icon;
+                this.player.GetComponent<PlayerController>().seedItem = item;
+                //this.player.GetComponent<PlayerController>().seedSlotBackground.GetComponent<Image>().sprite = item.GetComponent<Item>().icon;
                 this.player.GetComponent<Inventory>().ToggleDisplayInventory();
 
             }
