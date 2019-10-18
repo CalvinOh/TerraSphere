1d6881594e53878d997906c1e637ed20495bf990
TheQuestCrafter <thequestcrafter@gmail.com>
2019-10-17 21:59:21 -0500

    Icons Working - Context Based

---
+++ Tera Testing/Assets/ContextBasedUI.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContextBasedUI : MonoBehaviour
+{
+    [SerializeField]
+    private Image emptyIcon;
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    public Sprite seedIcon;
+    [SerializeField]
+    private Sprite waterIcon;
+    [SerializeField]
+    private Sprite harvestIcon;
+
+    [SerializeField]
+    private Collider detector;
+
+    private int alreadyActivated = 0;
+
+    private void Update()
+    {
+        CheckCollider();
+    }
+
+    private void CheckCollider()
+    {
+        if(detector != null)
+        {
+            if (detector.tag == "Plant")
+            {
+                emptyIcon.sprite = harvestIcon;
+            }
+            else if (detector.tag == "Seed")
+            {
+                emptyIcon.sprite = waterIcon;
+            }
+            else if (detector.tag == "Hole")
+            {
+                emptyIcon.sprite = seedIcon;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        detector = other;
+        alreadyActivated++;
+        if (alreadyActivated <= 0)
+        {
+            print("PopUpAnimStart");
+            animator.SetBool("AnimStart", true);
+        }
+        alreadyActivated++;
+        if (other.tag == "Plant")
+        {
+            emptyIcon.sprite = harvestIcon;
+        }
+        else if (other.tag == "Seed")
+        {
+            emptyIcon.sprite = waterIcon;
+        }
+        else if ( other.tag == "Hole")
+        {
+            emptyIcon.sprite = seedIcon;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        alreadyActivated--;
+        animator.SetBool("AnimEnd", true);
+        animator.SetBool("AnimStart", false);
+    }
+}
