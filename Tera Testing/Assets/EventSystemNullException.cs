0fec358ef9c62621f8f34b8157d60fad8ac5e2e5
TheQuestCrafter <thequestcrafter@gmail.com>
2019-10-28 20:58:14 -0500

    Updated Remaining Rockets

---
+++ Tera Testing/Assets/EventSystemNullException.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventSystemNullException : MonoBehaviour
+{
+    private EventSystem eventSystem;
+
+    [SerializeField]
+    [Tooltip("Gameobject you'd like to have selected as default.")]
+    private GameObject defaultSelected;
+
+    private void Awake()
+    {
+        eventSystem = FindObjectOfType<EventSystem>();
+    }
+    void Update()
+    {
+        if(eventSystem.currentSelectedGameObject == null)
+        {
+            eventSystem.SetSelectedGameObject(defaultSelected);
+        }
+    }
+}
