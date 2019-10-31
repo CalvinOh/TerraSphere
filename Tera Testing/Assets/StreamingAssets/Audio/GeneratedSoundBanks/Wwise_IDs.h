8aa1c2f20f7144e08cc3a02f8bb9354b352cb355
kruegerc96 <49258382+kruegerc96@users.noreply.github.com>
2019-10-30 20:29:22 -0500

    Wwise integration + Ambience
    Integrated Wwise and got ambience to play on start

---
+++ Tera Testing/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,70 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMBIENCE = 278617630U;
+        static const AkUniqueID PLAY_TS_SX_UNI_ENV_BUSH_THRU = 4229852413U;
+        static const AkUniqueID PLAY_TS_SX_UNI_INT_PLANT_HARVEST = 416558692U;
+        static const AkUniqueID PLAY_TS_SX_UNI_INT_SEED_PLANT = 2965097530U;
+        static const AkUniqueID PLAY_TS_SX_UNI_INT_SHOVEL = 2144010314U;
+        static const AkUniqueID PLAY_TS_SX_UNI_INT_WATER = 86867240U;
+        static const AkUniqueID PLAY_TS_SX_UNI_PLR_FOOTSTEPS = 550554089U;
+        static const AkUniqueID PLAY_TS_SX_UNI_UI_CLOSE = 4207799086U;
+        static const AkUniqueID PLAY_TS_SX_UNI_UI_NAVIGATE = 2658627103U;
+        static const AkUniqueID PLAY_TS_SX_UNI_UI_OPEN = 1124810858U;
+        static const AkUniqueID PLAY_TS_SX_UNI_UI_OXYGEN_LOW = 1567033245U;
+        static const AkUniqueID PLAY_TS_SX_UNI_UI_PLANT_CONSUME = 2262292382U;
+        static const AkUniqueID PLAY_TS_SX_UNI_UI_SELECT = 3919949730U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID TERRAFORMPERCENT = 2766866850U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
