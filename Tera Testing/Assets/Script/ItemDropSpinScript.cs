e7b693d7c5cb9ec84c44a519a76fccfdb29e246c
CalvinOh <calvinohwyekit@gmail.com>
2019-11-08 13:43:59 -0600

    New Script for items drop spin
    A simple script that makes the dropped items spin around the y axis

---
+++ Tera Testing/Assets/Script/ItemDropSpinScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDropSpinScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private float turnSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Rotate(0, turnSpeed, 0);
+    }
+}
