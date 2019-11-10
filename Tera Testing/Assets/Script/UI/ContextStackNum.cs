9ae64021f2fe95efff04c485115d5f957bd399d2
TheQuestCrafter <thequestcrafter@gmail.com>
2019-11-09 23:40:13 -0600

    Context UI and EmptyAlert Prefab

---
+++ Tera Testing/Assets/Script/UI/ContextStackNum.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContextStackNum : MonoBehaviour
+{
+    private PlayerController player;
+
+    [SerializeField]
+    private Text text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.currentlySelecting.tag == "Hole")
+        {
+            text.text = player.seedItem.GetComponent<Item>().stackNumber.ToString();
+        }
+        else
+        {
+            text.text = " ";
+        }
+    }
+}
---
+++ Tera Testing/Assets/Script/UI/UITextAlphaUpDown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextAlphaUpDown : MonoBehaviour
+{
+    [SerializeField]
+    private float changeSpeed = 1f;
+
+    private Text text;
+    private float currentColor;
+
+    private bool isFaded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        currentColor = text.color.a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentColor = text.color.a;
+        if (currentColor <= 1f)
+            isFaded = false;
+        else if (currentColor >= 0f)
+            isFaded = true;
+        if (isFaded)
+        {
+            text.CrossFadeAlpha(1, changeSpeed, false); 
+        }
+        else if (!isFaded)
+        {
+            text.CrossFadeAlpha(0, changeSpeed, false);
+        }
+    }
+}
