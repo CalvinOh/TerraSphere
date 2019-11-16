ef258bf6091f05e275a8b67ea58535438daaf183
kruegerc96 <49258382+kruegerc96@users.noreply.github.com>
2019-11-16 01:25:39 -0600

    added script for music evolution
    music will not evolve beyond desolate; unable to discern why. script is attached to Wwiseglobal and is running

---
+++ Tera Testing/Assets/Script/Audio/SetMusicEvolution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMusicEvolution : MonoBehaviour
+{
+    TerraBar t;
+
+    // Use this for initialization.
+    void Start()
+    {
+        AkSoundEngine.SetState("TerraformProgress", "Desolate");
+
+        t = (TerraBar)FindObjectOfType(typeof(TerraBar));
+    }
+
+    // Update is called once per frame.
+    void Update()
+    {
+        UpdateTerraformState();
+    }
+
+    void UpdateTerraformState()
+    {
+        //Debug.Log("MusicStateSwitcher Running");
+
+        if (t.barPercentage <= 5)
+        {
+            Debug.Log("music desolate");
+            AkSoundEngine.SetState("TerraformProgress", "Desolate");
+        }
+        else if ((t.barPercentage > 5) && (t.barPercentage < 10))
+        {
+            Debug.Log("music hopeful");
+            AkSoundEngine.SetState("TerraformProgress", "Hopeful");
+        }
+        else if (t.barPercentage >= 10)
+        {
+            Debug.Log("music cheerful");
+            AkSoundEngine.SetState("TerraformProgress", "Cheerful");
+        }
+    }
+}
