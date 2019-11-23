08fbeeeb7dd5b6cd8b81c7d96956b507be9d94ff
TheQuestCrafter <thequestcrafter@gmail.com>
2019-11-22 18:26:08 -0600

    Final 1.3 Changes

---
+++ Tera Testing/Assets/Script/RocketSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketSound : MonoBehaviour
+{
+    private float currentSceneTime;
+    [SerializeField]
+    private GameObject soundObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= currentSceneTime + .5f)
+        {
+            soundObj.SetActive(true);
+        }
+    }
+}
